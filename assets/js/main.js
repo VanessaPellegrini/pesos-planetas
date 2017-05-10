c8dcb199d7949288b8e310d2f63f93aaf86dbadd
VannPellegrini <vanessapellegrini@hotmail.com>
2017-05-09 23:09:10 -0300

    comienzo ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var contenedor = document.getElementById("contenedor");
+
+function calcular(){
+	var masa = document.getElementById("peso").value;
+	var gravedad = document.getElementById("planetas").value;
+	return alert("Tu peso sería " + (masa * gravedad).toFixed(2) + " kilos.");
+
+}
